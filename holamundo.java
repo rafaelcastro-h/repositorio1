37a249d3c349893ab947f160f86f90ab5c613457
Rafael Castro <rafaelcastro@ciencias.unam.mx>
2023-01-09 15:35:33 -0600

    Se agrego el archivo

---
+++ holamundo.java
@@ -0,0 +1,11 @@
+/** Este programa imprime Hola mundo en la cónsola
+ */
+
+
+
+public class holamundo{
+    public static void main(String args[]){
+        System.out.println("Hola mundo");
+    }
+
+}
