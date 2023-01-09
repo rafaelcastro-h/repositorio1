104d5e8d706d66f87058d5d764d1a4ad6007e94d
Rafael Castro <rafaelcastro@ciencias.unam.mx>
2023-01-09 15:58:47 -0600

    Se agrego un nuevo archivo

---
+++ Operacion.java
@@ -0,0 +1,18 @@
+/** Este programa imprime Hola mundo en la cónsola
+ */
+
+
+
+ public class Operacion{
+    public static void main(String args[]){
+        int numero1 = 7;
+        double numero2 = 5.6;
+        int resultado = numero1 + (int) numero2;
+        System.out.println("El resultado de la suma " + numero1 + " más " + numero2 + " es "  + resultado);
+
+
+
+
+    }
+
+}
